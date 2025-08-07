6abde51113f50a4be548612c861a078979992b7c
rightgoody <rightgoody@csu.fullerton.edu>
2025-08-06 22:16:22 -0700

    Pushing, calling working so far

--- .next/static/chunks/pages/_app-9de1e2a121c07928.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{6184:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(6804)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(6184),n(3922))),_N_E=_.O()}]);
