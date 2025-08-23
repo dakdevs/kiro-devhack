c9448755d58651b3964e4d680641bea28f8d0f62
Rightgoody <rightgoody@csu.fullerton.edu>
2025-08-23 13:27:50 -0700

    Create new elements to redirect user to /interview and new element at  /recruiter . recruiter page updated to reflect better with scope of idea in mind. woohoo!

---
+++ src/app/recruiter/post-job/page.tsx
@@ -0,0 +1,9 @@
+import { JobPostingWorkflow } from './_modules/job-posting-workflow';
+
+export default function PostJobPage() {
+  return (
+    <div className="min-h-screen">
+      <JobPostingWorkflow />
+    </div>
+  );
+}
